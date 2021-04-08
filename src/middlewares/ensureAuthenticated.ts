4906c359306f140cd2501e3c2a6f884b48372e91
EvandroRBR <evandro.braga01@fatec.sp.gov.br>
2021-04-08 16:10:16 -0300

    Testes de autenticaçao e de criaçao de categorias

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
+import { AppError } from '../errors/AppError';
 import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
 
 interface IPayload {
@@ -15,7 +16,7 @@ export async function ensureAuthenticated(
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error('JWT token is missing.');
+    throw new AppError('JWT token is missing.', 401);
   }
 
   const [, token] = authHeader.split(' ');
@@ -30,14 +31,16 @@ export async function ensureAuthenticated(
 
     const user = await usersRepository.findById(user_id);
 
-    console.log(user_id);
-
     if (!user) {
-      throw new Error('User does not exists.');
+      throw new AppError('User does not exists.', 401);
     }
+
+    request.user = {
+      id: user.id,
+    };
 
     next();
   } catch {
-    throw new Error('Invalid token!');
+    throw new AppError('Invalid token!', 401);
   }
 }
--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
 import { injectable, inject } from 'tsyringe';
 
+import { AppError } from '../../../../errors/AppError';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
@@ -28,13 +29,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.');
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.');
     }
 
     const token = sign({}, '4d0b0db850bc47ffa3e07bc35b2ac6fb', {
